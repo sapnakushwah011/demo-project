3304001fea751e052f2a05e3b6da1bca163fed27
Tc-Sapna <sapna@techcarrel.com>
2024-10-09 12:37:58 +0530

    updated example test

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -1,21 +1,19 @@
 // @ts-check
-const { test, expect } = require("@playwright/test");
+const { test, expect } = require('@playwright/test');
 
-// test('has title', async ({ page }) => {
-//   await page.goto('https://playwright.dev/');
+test('has title', async ({ page }) => {
+  await page.goto('https://playwright.dev/');
 
-//   // Expect a title "to contain" a substring.
-//   await expect(page).toHaveTitle(/Playwright/);
-// });
+  // Expect a title "to contain" a substring.
+  await expect(page).toHaveTitle(/Playwright/);
+});
 
-for (var i = 0; i < 100; i++) {
-  test('test ' + i, async ({ page }) => {
-    await page.goto("https://playwright.dev/");
+test('get started link', async ({ page }) => {
+  await page.goto('https://playwright.dev/');
 
-    // Click the get started link.
-    await page.getByRole("link", { name: "Get started" }).click();
+  // Click the get started link.
+  await page.getByRole('link', { name: 'Get started' }).click();
 
-    // Expects page to have a heading with the name of Installation.
-    await expect(page.getByRole("heading", { name: "Installation" })).toBeVisible();
-  });
-}
+  // Expects page to have a heading with the name of Installation.
+  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+});
