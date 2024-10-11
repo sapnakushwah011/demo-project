c9b519d02cff7c0b519405b306063c9621b69c2d
Tc-Sapna <sapna@techcarrel.com>
2024-10-11 11:06:11 +0530

    navbar updated

--- src/componentTestMapping.js
+++ src/componentTestMapping.js
@@ -1,10 +1,10 @@
 // componentTestMapping.js
 
 const componentTestMap = {
-  '/src/Components/Login.jsx': '/tests/Login.spec.js',
-  '/src/Components/Navbar.jsx': '/tests/Navbar.spec.js',
-  '/src/Components/GlobalDropdown.jsx': '/tests/GlobalDropdown.spec.js',
-  '/src/Components/PaginateAutoComplete.jsx': '/tests/PaginateAutoComplete.spec.js',
+  'src/Components/Login.jsx': '/tests/Login.spec.js',
+  'src/Components/Navbar.jsx': '/tests/Navbar.spec.js',
+  'src/Components/GlobalDropdown.jsx': '/tests/GlobalDropdown.spec.js',
+  'src/Components/PaginateAutoComplete.jsx': '/tests/PaginateAutoComplete.spec.js',
   // Add more mappings as needed
 };
 
