05d9a05a0427b1dbb1137b7de5f219705c270a9a
Tc-Sapna <sapna@techcarrel.com>
2024-10-11 10:54:36 +0530

    navbar test

--- tests/Navbar.spec.js
+++ tests/Navbar.spec.js
@@ -1,4 +1,15 @@
 import { test, expect } from "@playwright/test";
+import { exec } from 'child_process';
+
+test.setTimeout(60000);
+
+test.beforeAll(async () => {
+    // Start the server
+    exec('npm start');
+  
+    // Optionally wait for server to be ready
+    await new Promise(r => setTimeout(r, 10000)); // Wait 10 seconds
+  });
 
 test("test", async ({ page }) => {
    await page.goto("http://localhost:3000/");
